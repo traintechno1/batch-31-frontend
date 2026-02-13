17a937166a651242da30d5d434a713624d72567c
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2026-02-13 10:08:43 +0530

    Function changes

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+// var a = 100;
+// var b = 200;
+
+// function add(value1, value2){
+//     return value1 + value2;
+// }
+
+// function expression
+// var add = function(value1, value2){
+//     return value1 + value2;
+// }
+
+// var add = (value1, value2) => { 
+//     return value1 + value2;
+// }
+
+// var add = (value1, value2) => value1 + value2;
+
+// console.log(add(100, 150));
+// console.log(add(200, 400));
+// console.log(add(50, 150));
+
+// (() => console.log("I am called"))();
