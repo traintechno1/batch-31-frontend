365e09e426f430f16a7d848f6b07c1269dbd2676
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2026-02-25 10:04:44 +0530

    JS String methods

---
+++ js/string.js
@@ -0,0 +1,59 @@
+// let str = "Anubhav is a 'Java' dev";
+// let str1 = 'Anubhav is a "full stack" dev';
+// let str2 = `Anubhav is a 'full' "stack" dev`;
+
+// console.log(typeof str);
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(str);
+// console.log(str2);
+// console.log(str1);
+
+// let str = "Test";
+// let str2 = "User";
+
+// let str3 = str + " " + str2;
+// console.log(str3);
+
+// let num = 100;
+// let num2 = "100";
+
+// let result = num * num2;
+
+// console.log(result);
+
+// let str = "this is a new programming lecture that will help us learning programming";
+let pName = "Rahul Prasad";
+
+// for (const c of pName) {
+//     console.log(c);
+// }
+// console.log(pName.length);
+// console.log(pName.charAt(7));
+// console.log(pName.charCodeAt(7));
+// console.log(pName.concat(" is a Java dev"));
+// console.log(pName.endsWith("a"));
+// console.log(pName.includes("hul"));
+// console.log(pName.indexOf("hul"));
+// console.log(pName.lastIndexOf("a"));
+// console.log([...str.matchAll(/programming/gi)]);
+
+// console.log(pName.padEnd(20, "#"));
+// console.log(pName.padStart(20, "-"));
+// console.log(pName.repeat(4));
+// console.log(pName.slice(4, 9));
+// console.log(pName.startsWith("R"));
+// console.log(pName.substring(3, 8));
+// console.log(pName.toLowerCase());
+// console.log(pName.toUpperCase());
+// console.log(pName.trim());
+// console.log(pName.trimEnd());
+// console.log(pName.trimStart());
+// console.log(pName.replaceAll("a", "k"));
+console.log(pName.split(" "));
+
+localStorage.setItem("Name", "Hemant");
+localStorage.setItem("key", "89776128B*gbg87HGBd9agha");
+localStorage.removeItem('key');
+
+console.log(pName);
